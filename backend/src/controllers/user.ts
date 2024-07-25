d58797916b5d69881b1334e0afa742ecd3a380ad
TejasK30 <tejas.k6389@gmail.com>
2024-07-26 00:28:51 +0530

    added password hashing

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { z } from "zod"
+import bcrypt from "bcrypt"
 import UserModel from "../models/User"
 
 const registerSchema = z.object({
@@ -7,23 +8,32 @@ const registerSchema = z.object({
   email: z.string().email({ message: "Inavalid email format !" }),
   password: z.string(),
 })
+
 export const registerController = async (req: Request, res: Response) => {
   registerSchema.safeParse(req.body)
 
+  const { username, email, password } = req.body
+
   try {
     let userexists = await UserModel.findOne({
-      email: req.body.email,
+      email: email,
     })
 
     if (userexists) {
       return res.status(400).json({ message: "User already exists" })
     }
-    
-    const user = new UserModel(req.body)
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = new UserModel({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    })
 
     await user.save()
 
-    return res.json({message: "User registration successful"})
+    return res.json({ message: "User registration successful" })
   } catch (error) {
     if (error instanceof z.ZodError) {
       res.status(400).json({
@@ -34,6 +44,7 @@ export const registerController = async (req: Request, res: Response) => {
       res.status(500).json({
         message: "Internal server error",
       })
+      console.log(error)
     }
   }
 }
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,6 +1,11 @@
 import mongoose, { model, Schema } from "mongoose";
 
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   email: {
     type: String,
     required: true,
