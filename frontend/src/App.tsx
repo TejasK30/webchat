17b0546899d5c932cc867aa5644f67a0a1dde3a0
TejasK30 <tejas.k6389@gmail.com>
2024-08-28 01:10:37 +0530

    protected route for chat.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Chat from "./components/Chat"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Profile from "./pages/Profile"
+import useUserContext from "./hooks/useUserContext"
 
 const App = () => {
 
+  const { isLoggedin } = useUserContext()
+  
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Chat />} />
+        <Route path="/" element={isLoggedin ? <Chat /> : <Navigate to="/login" />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
